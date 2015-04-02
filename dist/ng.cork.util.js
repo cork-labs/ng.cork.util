b002e6e3a9e791ec4ee00d1132283b40b12a93df
Andre Torgal <andre.torgal@gmail.com>
2015-04-02 15:32:42 +0100

    chore(release): v0.0.4

--- dist/ng.cork.util.js
+++ dist/ng.cork.util.js
@@ -1,5 +1,5 @@
 /**
- * ng.cork.util - v0.0.3 - 2015-04-01
+ * ng.cork.util - v0.0.4 - 2015-04-02
  * https://github.com/cork-labs/ng.cork.util
  *
  * Copyright (c) 2015 Cork Labs <http://cork-labs.org>
